dd2f6e5f94b0bfda0b6954f17cd820f3560c6435
teeque <teeque87@gmail.com>
2024-03-14 10:42:48 +0100

    first commits

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+app.use('/static', express.static(__dirname + '/public'))
+
+// Database Init
+main().catch((err) => console.log(err))
+async function main() {
+	await mongoose.connect(process.env.MONGOOSE_DATABASE_STRING)
+	console.log('Database running...')
+}
+
+app.get('/', function (req, res) {
+	res.send(' ')
+})
+
+app.listen(3000)
